e2330b3ac267de8d21651125a187650e568523d9
Joel Semar <joel.semar@appiction.com>
2010-10-01 12:41:05 -0500

    catch all loggin exceptions

--- webservice_tools/logging/middleware.py
+++ webservice_tools/logging/middleware.py
@@ -6,14 +6,18 @@ class LoggingMiddleware(object):
         if 'text/html' in response['Content-Type']:
             return response
         
-        log = "-------------------------------------\n%(request)s\nURL: %(url)s\nRESPONSE\n%(response)s\n-------------------------------------\n" 
+        log = "-------------------------------------\n%(request)s\nHANDLER: %(method)s %(url)s\nRESPONSE\n%(response)s\n-------------------------------------\n" 
         msg = ""
-        if request.GET:
-            msg += "REQUEST GET: %s" % toDict(request.GET)
-        if request.POST:
-            msg += "REQUEST POST: %s" % toDict(request.POST)
+        try:
+            if request.GET:
+                msg += "REQUEST GET: %s" % toDict(request.GET)
+            if request.POST:
+                msg += "REQUEST POST: %s" % toDict(request.POST)
+        except:
+            pass
             
         logging.debug(log % {'request': msg,
+                             'method': request.method,
                              'response': response,
                              'url': request.path})
         return response
