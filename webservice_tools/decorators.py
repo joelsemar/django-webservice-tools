bfd5d174f8ae511a5b583b46f4e74d0a4eea0e7e
Joel Semar <joel.semar@appiction.com>
2010-11-18 11:24:12 -0600

    login decorator

---
+++ webservice_tools/decorators.py
@@ -0,0 +1,17 @@
+
+from response_util import ResponseObject
+
+def login_required(fn):
+    def inner(*args, **kwargs):
+        
+        response = ResponseObject(dataFormat=kwargs.get('dataFormat', 'json'))
+        try:
+            request = [a for a in args if hasattr(a, 'user')][0]
+        except IndexError:
+            return response.send(errors="Login required method called without request object", status=500)
+        if request.user.is_authenticated():
+            return fn(*args, **kwargs)
+        
+        return response.send(errors='401 -- Unauthorized', status=401)
+    
+    return inner
