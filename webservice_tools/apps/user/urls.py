3ec92a8d280a05cc163dea0f200cf3a3211a5963
Joel Semar <semarjt@gmail.com>
2011-04-19 15:57:46 -0500

    fix social test

--- webservice_tools/apps/user/urls.py
+++ webservice_tools/apps/user/urls.py
@@ -1,11 +1,4 @@
 from django.conf.urls.defaults import *
-from webservice_tools.utils import Resource
-from webservice_tools.apps.user.handlers import FacebookHandler, GenericUserHandler, LoginHandler, TwitterHandler 
 urlpatterns = patterns('',
-    (r'^/?$', Resource(GenericUserHandler)),
-    (r'^login/?$', Resource(LoginHandler)),
-    (r'^logout/?$', Resource(LoginHandler)),
-    (r'^(?P<network>facebook)/?$', Resource(FacebookHandler)),
-    (r'^(?P<network>twitter)/?$', Resource(TwitterHandler)),
-    
+   
 )
--- webservice_tools/urls.py
+++ webservice_tools/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
 from webservice_tools.views import *
+from django.conf import settings
 urlpatterns = patterns('webservice_tools.views',
     ('geo$', Resource(GeoHandler)),
     ('resetpass/?$', Resource(ResetPassHandler)),
     ('locations/?$', Resource(PlacesHandler)),
     ('amialive/?$', 'amialive'),
     ('docs/?$', Resource(DocHandler)),
-    ('socialtest/?$', direct_to_template, {'template': 'socialtest.html'}),
+    (r'apiconsole/?$', direct_to_template, {'template': 'apiconsole.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
+    (r'^socialtest$', direct_to_template, {'template': 'socialtest.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
+    
     )
