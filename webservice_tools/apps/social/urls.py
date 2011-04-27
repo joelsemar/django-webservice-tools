52702d2031373805de65088c4ffbef3e8708e98d
Joshua Semar <joshua.semar@appiction.com>
2011-04-27 08:29:59 -0500

    Preliminary Social Changes, utils.is_num addition

--- webservice_tools/apps/social/urls.py
+++ webservice_tools/apps/social/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import *
 from webservice_tools.utils import Resource
-from webservice_tools.apps.social.handlers import FacebookHandler, TwitterHandler 
+from webservice_tools.apps.social.handlers import SocialRegisterHandler, SocialPostHandler
+from webservice_tools.apps.social.handlers import SocialFriendHandler, SocialCallbackHandler
 urlpatterns = patterns('',
-    (r'^(?P<network>facebook)/?$', Resource(FacebookHandler)),
-    (r'^(?P<network>twitter)/?$', Resource(TwitterHandler)),
+    (r'^register/?$', Resource(SocialRegisterHandler)),
+    (r'^post/?$', Resource(SocialPostHandler)),
+    (r'^friends/?$', Resource(SocialFriendHandler)),
+    (r'^callback/(?P<network>)/?$', Resource(SocialCallbackHandler)),
 )
--- webservice_tools/urls.py
+++ webservice_tools/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('webservice_tools.views',
     ('locations/?$', Resource(PlacesHandler)),
     ('amialive/?$', 'amialive'),
     ('docs/?$', Resource(DocHandler)),
-    (r'apiconsole/?$', direct_to_template, {'template': 'apiconsole.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
-    (r'^socialtest$', direct_to_template, {'template': 'socialtest.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
-    
+    (r'^apiconsole/?$', direct_to_template, {'template': 'apiconsole.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
+    (r'^socialtest/?$', direct_to_template, {'template': 'socialtest.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
+    (r'^', Resource(DocHandler)),
     )
