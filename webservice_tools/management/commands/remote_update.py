9961ae80c84483104cfcc248c406a5528e50aa22
Joel Semar <semarjt@gmail.com>
2011-05-02 11:24:32 -0500

    stuff

---
+++ webservice_tools/management/commands/remote_update.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+import pexpect
+from django.conf import settings
+class Command(BaseCommand):
+    """
+    """
+    def handle(self, *args, **options):
+        try:
+            username = args[0]
+        except IndexError:
+            print "Error: No username specified for login to remote server."
+            return
+        server_name = settings.SERVER_NAME
+        try:
+            domain = args[1]
+        except IndexError:
+            domain = 'staging.appiction.com'
+        command = 'cd /var/www/%s && sudo -u www-data git reset --hard && sudo -u www-data git pull && sudo ./manage.py migrate && sudo /etc/init.d/apache2 restart' % server_name
+        p = pexpect.spawn('ssh -t %s@%s %s' % (username, domain, command))
+        p.interact()
