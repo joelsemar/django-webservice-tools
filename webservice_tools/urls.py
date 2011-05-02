9b9f844dbcaeb3422f2a4e439dd6323259da1635
Joel Semar <semarjt@gmail.com>
2011-05-02 11:23:12 -0500

    stuff

--- webservice_tools/urls.py
+++ webservice_tools/urls.py
@@ -3,11 +3,11 @@ from webservice_tools.utils import Resource
 from webservice_tools.views import *
 from django.conf import settings
 urlpatterns = patterns('webservice_tools.views',
-    ('geo$', Resource(GeoHandler)),
-    ('resetpass/?$', Resource(ResetPassHandler)),
-    ('locations/?$', Resource(PlacesHandler)),
-    ('amialive/?$', 'amialive'),
-    ('docs/?$', Resource(DocHandler)),
+    (r'geo$', Resource(GeoHandler)),
+    (r'resetpass/?$', Resource(ResetPassHandler)),
+    (r'locations/?$', Resource(PlacesHandler)),
+    (r'amialive/?$', 'amialive'),
+    (r'docs/?$', Resource(DocHandler)),
     (r'^apiconsole/?$', direct_to_template, {'template': 'apiconsole.html', 'extra_context': {'baseURL': '/%s/' % settings.SERVER_NAME}}),
-    (r'^socialtest/?$', direct_to_template, {'template': 'socialtest.html', 'extra_context': {'baseURL': '/%s/social/' % settings.SERVER_NAME}}),
+    
     )
