6274f0de6ff358b507c66aa3d471513e8bb01c0d
Joel Semar <semarjt@gmail.com>
2011-06-02 08:08:09 -0500

    fix friends url;

--- webservice_tools/apps/friends/urls.py
+++ webservice_tools/apps/friends/urls.py
@@ -3,8 +3,9 @@ from webservice_tools.utils import Resource
 from webservice_tools.apps.friends.handlers import FriendsHandler, FriendRequestHandler, GroupHandler, GroupsHandler 
 urlpatterns = patterns('',
     (r'^(?P<id>[\d]+)/?$', Resource(FriendsHandler)),
-    (r'^requests/?$', Resource(FriendRequestHandler)),
+    (r'^requests/(?P<id>[\d]+)/?$', Resource(FriendRequestHandler)),
     (r'^requests/user/(?P<id>[\d]+)/?$', Resource(FriendRequestHandler)),
+    (r'^requests/?$', Resource(FriendRequestHandler)),
     (r'^group/(?P<id>[\d]+)/?$', Resource(GroupHandler)),
     (r'^group/?$', Resource(GroupHandler)),
     (r'^group/(?P<group_id>[\d]+)/friend/(?P<friend_id>)/?$', Resource(GroupHandler)),
