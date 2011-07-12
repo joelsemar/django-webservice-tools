0795cefa2189b2f73c2d6d5cb10906091625dac4
Joel Semar <semarjt@gmail.com>
2011-07-12 14:29:44 -0500

    data format stuff

--- webservice_tools/middleware/response.py
+++ webservice_tools/middleware/response.py
@@ -14,33 +14,17 @@ class ProvideResponse(object):
         """
         data_format = None
         accept_header = request.META.get('HTTP_ACCEPT', '')
-        if 'text/xml' in accept_header and 'html' not in accept_header:
-            data_format = 'xml'
-        elif 'application/json' in accept_header:
-            data_format = 'json'
         
-        data_format = data_format or request.GET.get('format') or request.POST.get('format') 
-        
-        if ('html' in accept_header or
-            #'*/*' in accept_header or 
-            'admin' in request.path or 
-            'static' in request.path) and not data_format:
+        if not hasattr(view, 'callmap'):
             return None
+        
+        request.handler_id = str(view.handler.__class__)
         
         if getattr(settings, 'MESSAGES_ENABLED', False):
             kwargs['response'] = ResponseObject(request=request)
         else:
             kwargs['response'] = ResponseObject()
-        if request.META.get("HTTP_SHOW_DOC"):
-            if hasattr(view, 'callmap'):
-                doc = getattr(view.handler, view.callmap[request.method]).func_doc
-            else:
-                doc = view.func_doc
-            if data_format == 'json' and doc:
-                doc = doc.replace('\n', '<br/>')
-            kwargs['response'].doc = doc
-        if hasattr(view, 'callmap'):
-            request.handler_id = str(view.handler.__class__)
+         
         
         return None
 
