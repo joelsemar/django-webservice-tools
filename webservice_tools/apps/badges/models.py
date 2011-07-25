4f6f3c96f6e54afbddd2909fdcac9b52d931609b
Joel Semar <semarjt@gmail.com>
2011-07-25 11:19:54 -0500

    paging info

--- webservice_tools/apps/badges/models.py
+++ webservice_tools/apps/badges/models.py
@@ -41,7 +41,11 @@ class BadgeModel(models.Model):
         for image in images:
             ret[image.image_type] = image.image.url
         return ret
-
+    
+    
+    def info(self, profile):
+    
+    
 def badge_upload_to(instance, filename):
     return os.path.join('images/badges', '%s-%s.png' % (datetime.datetime.utcnow().strftime('%M%S%f'),filename.split('.')[0]))
 
