4d7c0d5ee8b7ca9199bb54a5c1d14d995e7a6943
Joshua Semar <joshua.semar@appiction.com>
2011-08-09 09:14:45 -0500

    setup.py tweaks

--- webservice_tools/lib/py_ffmpeg/setup.py
+++ webservice_tools/lib/py_ffmpeg/setup.py
@@ -5,35 +5,12 @@ from distutils.extension import Extension
 from os.path import join as path_join
 from sys import platform
 
-## Try to locate source if necessary
-##
-#sources = ['py_ffmpeg.c']
-##headers = []
-#for root, dir, files in os.walk('./include'):
-#    for file in files:
-#        if '.c' in file:
-#            sources.append(os.path.join(root,file))
-# #       if '.h' in file:
-#  #          headers.append(os.path.join(root,file))
-#
-#ext_modules=[ Extension('py_ffmpeg', sources=sources,
-#   #                     headers = headers,
-#                   include_dirs = ['./include/'],) 
-#                ]
-# 
-#
-#setup(
-#    name = 'test',
-#    version = ".1.0",
-#    ext_modules = ext_modules
-#)
-
 setup(
         name="py_ffmpeg",
         ext_modules=[ 
           Extension("py_ffmpeg", ["py_ffmpeg.c"],
-              include_dirs=["/usr/include/libavcodec/", "/usr/include/libavformat", "/usr/include/libavutil", "/usr/include/libswscale"],
-              library_dirs=["/usr/lib/", "/usr/local/lib"],
+              include_dirs=["/usr/local/include/libavcodec/", "/usr/local/include/libavformat", "/usr/local/include/libavutil", "/usr/local/include/libswscale"],
+              library_dirs=["/usr/local/lib/", "/usr/lib"],
               libraries=["avformat", "avcodec", "swscale", "avutil"])
           ],
         #cmdclass={'build_ext': build_ext},
